0863e9a9776914990e7c2506e160e96e280f1851
CCXia1997 <879139909@qq.com>
2019-11-21 21:56:38 +0800

    c#

---
+++ s7-300/s7-300/Form1.Designer.cs
@@ -0,0 +1,349 @@
+﻿namespace s7_300
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbPlcAddress = new System.Windows.Forms.TextBox();
+            this.btnLink = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbRack = new System.Windows.Forms.TextBox();
+            this.tbSlot = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.cbDataType = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.tbWriteData = new System.Windows.Forms.TextBox();
+            this.tbReadData = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tbDataAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.pingState = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnTest = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pingState)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "PLC地址";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // tbPlcAddress
+            // 
+            this.tbPlcAddress.Location = new System.Drawing.Point(69, 23);
+            this.tbPlcAddress.Name = "tbPlcAddress";
+            this.tbPlcAddress.Size = new System.Drawing.Size(163, 21);
+            this.tbPlcAddress.TabIndex = 1;
+            // 
+            // btnLink
+            // 
+            this.btnLink.Location = new System.Drawing.Point(69, 129);
+            this.btnLink.Name = "btnLink";
+            this.btnLink.Size = new System.Drawing.Size(75, 23);
+            this.btnLink.TabIndex = 2;
+            this.btnLink.Text = "连接";
+            this.btnLink.UseVisualStyleBackColor = true;
+            this.btnLink.Click += new System.EventHandler(this.btnLink_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(157, 129);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "断开";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "机架号";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 91);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "插槽号";
+            // 
+            // tbRack
+            // 
+            this.tbRack.Location = new System.Drawing.Point(69, 56);
+            this.tbRack.Name = "tbRack";
+            this.tbRack.Size = new System.Drawing.Size(163, 21);
+            this.tbRack.TabIndex = 6;
+            // 
+            // tbSlot
+            // 
+            this.tbSlot.Location = new System.Drawing.Point(69, 88);
+            this.tbSlot.Name = "tbSlot";
+            this.tbSlot.Size = new System.Drawing.Size(163, 21);
+            this.tbSlot.TabIndex = 7;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.cbDataType);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.btnWrite);
+            this.groupBox1.Controls.Add(this.btnRead);
+            this.groupBox1.Controls.Add(this.tbWriteData);
+            this.groupBox1.Controls.Add(this.tbReadData);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.tbDataAddress);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Location = new System.Drawing.Point(45, 183);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(256, 182);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "读写区";
+            // 
+            // cbDataType
+            // 
+            this.cbDataType.FormattingEnabled = true;
+            this.cbDataType.Items.AddRange(new object[] {
+            "Bit",
+            "Byte",
+            "Word",
+            "DWord",
+            "Int",
+            "DInt"});
+            this.cbDataType.Location = new System.Drawing.Point(69, 26);
+            this.cbDataType.Name = "cbDataType";
+            this.cbDataType.Size = new System.Drawing.Size(167, 20);
+            this.cbDataType.TabIndex = 9;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(10, 29);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 12);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "数据类型";
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(161, 144);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 7;
+            this.btnWrite.Text = "写";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(69, 144);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 6;
+            this.btnRead.Text = "读";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // tbWriteData
+            // 
+            this.tbWriteData.Location = new System.Drawing.Point(69, 108);
+            this.tbWriteData.Name = "tbWriteData";
+            this.tbWriteData.Size = new System.Drawing.Size(167, 21);
+            this.tbWriteData.TabIndex = 5;
+            // 
+            // tbReadData
+            // 
+            this.tbReadData.Location = new System.Drawing.Point(69, 81);
+            this.tbReadData.Name = "tbReadData";
+            this.tbReadData.Size = new System.Drawing.Size(167, 21);
+            this.tbReadData.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 111);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 3;
+            this.label6.Text = "写入数据";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "读取数据";
+            // 
+            // tbDataAddress
+            // 
+            this.tbDataAddress.Location = new System.Drawing.Point(69, 52);
+            this.tbDataAddress.Name = "tbDataAddress";
+            this.tbDataAddress.Size = new System.Drawing.Size(167, 21);
+            this.tbDataAddress.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "数据地址";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.tbPlcAddress);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.tbSlot);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.tbRack);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.btnLink);
+            this.groupBox2.Controls.Add(this.btnClose);
+            this.groupBox2.Location = new System.Drawing.Point(45, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(256, 165);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "连接区";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.btnTest);
+            this.groupBox3.Controls.Add(this.pingState);
+            this.groupBox3.Controls.Add(this.label7);
+            this.groupBox3.Location = new System.Drawing.Point(318, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(99, 353);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "状态区";
+            // 
+            // pingState
+            // 
+            this.pingState.BackColor = System.Drawing.Color.Red;
+            this.pingState.Location = new System.Drawing.Point(20, 51);
+            this.pingState.Name = "pingState";
+            this.pingState.Size = new System.Drawing.Size(57, 52);
+            this.pingState.TabIndex = 1;
+            this.pingState.TabStop = false;
+            this.pingState.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 24);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "连接状态";
+            // 
+            // btnTest
+            // 
+            this.btnTest.Location = new System.Drawing.Point(12, 315);
+            this.btnTest.Name = "btnTest";
+            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.TabIndex = 2;
+            this.btnTest.Text = "测试按钮";
+            this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(445, 398);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Name = "Form1";
+            this.Text = "s7-300 读写测试";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pingState)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbPlcAddress;
+        private System.Windows.Forms.Button btnLink;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbRack;
+        private System.Windows.Forms.TextBox tbSlot;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.TextBox tbWriteData;
+        private System.Windows.Forms.TextBox tbReadData;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tbDataAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pingState;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.ComboBox cbDataType;
+        private System.Windows.Forms.Button btnTest;
+    }
+}
+
---
+++ s7-300/s7-300/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using S7.Net;
+
+namespace s7_300
+{
+    public partial class Form1 : Form
+    {
+        //创建一个s7-300PLC对象
+        Plc s7300=null;
+        //状态区-连接状态
+        bool status = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        //连接PLC
+        private void btnLink_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                s7300 = new Plc(CpuType.S7300, tbPlcAddress.Text, short.Parse(tbRack.Text), short.Parse(tbSlot.Text));
+                s7300.Open();
+                status = true;
+                pingState.BackColor = Color.Green;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        //断开PLC
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (s7300 != null)
+                {
+                    s7300.Close();
+                    status = false;
+                    pingState.BackColor = Color.Red;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //读取PLC中的数据
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            if (status)
+            {
+                try
+                {
+                    object result = s7300.Read(tbDataAddress.Text);
+                    tbReadData.Text = string.Format("{0}", result.ToString());
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, ex.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("PLC未连接","警告");
+            }
+        }
+
+        //往PLC写入数据
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            if (status)
+            {
+                try
+                {
+                    string writeData = tbWriteData.Text;
+                    //根据选定的数据类型变换类型
+                    //Bit
+                    if (cbDataType.SelectedIndex == 0)
+                    {
+                        s7300.Write(tbDataAddress.Text, bool.Parse(writeData));
+                    }
+                    //Byte
+                    else if (cbDataType.SelectedIndex == 1)
+                    {
+                        s7300.Write(tbDataAddress.Text, byte.Parse(writeData));
+                    }
+                    //Word,16位无符号整数用ushort
+                    else if (cbDataType.SelectedIndex == 2)
+                    {
+                        s7300.Write(tbDataAddress.Text, ushort.Parse(writeData));
+                    }
+                    //DWord,32位无符号整数用uint
+                    else if (cbDataType.SelectedIndex == 3)
+                    {
+                        s7300.Write(tbDataAddress.Text, uint.Parse(writeData));
+                    }
+                    //Int,16位整数用short
+                    else if (cbDataType.SelectedIndex == 4)
+                    {
+                        s7300.Write(tbDataAddress.Text,short.Parse(writeData).ConvertToUshort());
+                    }
+                    //DInt,32位整数用int
+                    else if(cbDataType.SelectedIndex==5)
+                    {
+                        s7300.Write(tbDataAddress.Text,int.Parse(writeData));
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, ex.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                MessageBox.Show("PLC未连接", "警告");
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        //测试按钮
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            if (status)
+            {
+                try
+                {
+                    //Q2.0=1
+                    //byte[] result= { 0,1 };
+                    //s7300.WriteBytes(DataType.Output,1,1,result);
+                    byte[] result = s7300.ReadBytes(DataType.DataBlock, 1, 2, 2);
+                    StringBuilder logMessage = new StringBuilder("读取到的结果是:");
+                    foreach(byte data in result)
+                    {
+                        logMessage = logMessage.AppendFormat(data.ToString()+"  ");
+                    }
+                    MessageBox.Show(logMessage.ToString());
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(this, ex.Message, "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+    }
+}
